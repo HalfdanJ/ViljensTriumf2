cde3fd02532eb31211fc5d3d554b228d0a17f7a5
Jonas / Viljens Triumf <jonas@halfdanj.dk>
2012-10-25 01:53:14 +0200

    video chroma blabla

---
+++ ViljensTriumf/MyAVPlayerItem.h
@@ -0,0 +1,13 @@
+//
+//  MyAVPlayerItem.h
+//  ViljensTriumf
+//
+//  Created by Jonas on 10/25/12.
+//  Copyright (c) 2012 Jonas. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface MyAVPlayerItem : AVPlayerItem
+
+@end
---
+++ ViljensTriumf/MyAVPlayerItem.m
@@ -0,0 +1,13 @@
+//
+//  MyAVPlayerItem.m
+//  ViljensTriumf
+//
+//  Created by Jonas on 10/25/12.
+//  Copyright (c) 2012 Jonas. All rights reserved.
+//
+
+#import "MyAVPlayerItem.h"
+
+@implementation MyAVPlayerItem
+
+@end
